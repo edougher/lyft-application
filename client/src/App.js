a6c088ff3b4b81604eba6308d3f829adda510afc
Aaron Dougher <aaron@Aarons-MacBook-Pro.local>
2021-06-22 00:50:53 -0500

    added material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import {
+  Container,
+  Paper,
+  Typography,
+  Button,
+  Input,
+  Grid,
+} from "@material-ui/core";
+import useStyles from "./styles";
 
 function App(props) {
   let [response, setResponse] = useState({});
   let [postData, setPostData] = useState("");
+  let classes = useStyles();
 
   const body = { string_to_cut: `${postData}` };
 
@@ -10,22 +20,21 @@ function App(props) {
     const reqObj = {
       method: "POST",
       body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json" }
+      headers: { "Content-Type": "application/json" },
     };
 
     try {
-      const response = await fetch("https://cut-string-lyft.herokuapp.com/test", reqObj);
-      const data = await response.json()
-      console.log(data)
-      setResponse(data)
+      const response = await fetch(
+        "https://cut-string-lyft.herokuapp.com/test",
+        reqObj
+      );
+      const data = await response.json();
+      console.log(data);
+      setResponse(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
-
-  //useEffect(() => {
-  //  fetchFromApi()
-  //});
 
   const handleChange = (e) => {
     setPostData(e.target.value);
@@ -33,21 +42,34 @@ function App(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetchFromApi()
+    fetchFromApi();
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => handleChange(e)}
-        value={postData}
-      ></input>
-      <button type="button" onClick={handleSubmit}>
-        Fetch Response
-      </button>
-      <h2>{JSON.stringify(response)}</h2>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+      <Typography>{JSON.stringify(body)}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Input
+            placeholder="Enter a string"
+            fullWidth
+            type="text"
+            onChange={(e) => handleChange(e)}
+            value={postData}
+          />
+          <Button
+            className={classes.submit}
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Submit
+          </Button>
+          <Typography>{JSON.stringify(response)}</Typography>
+        </form>
+      </Paper>
+    </Container>
   );
 }
 
---
+++ client/src/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: theme.spacing(6),
+  },
+  
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(2),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
